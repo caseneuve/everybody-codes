ed669d9e06393532744665fbc54091aacfb10652
Piotr Kaznowski <piotr@kazno.dev>
2024-11-21 23:07:28 +0100

    Removed leftover file

--- ec2024/tools/input.clj
+++
@@ -1,8 +0,0 @@
-(ns tools.io
-  (:require [clojure.string :refer [split-lines trim]]))
-
-(defn file->str [part file]
-  (trim (slurp (format "quest%02d/%s" (Integer. part) file))))
-
-(defn file->lines [part file]
- (split-lines (file->str part file)))
